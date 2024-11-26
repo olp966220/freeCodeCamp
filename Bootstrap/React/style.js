26184bf6bc42287c74b96377d56d69538892fe75
choe <596232711@qq.com>
2024-11-26 09:30:14 +0800

    style

---
+++ Bootstrap/React/style.js
@@ -0,0 +1,8 @@
+/* <div style={{color: "yellow", fontSize: 16}}>Mellow Yellow</div> */
+class Colorful extends React.Component {
+  render() {
+    return (
+      <div style={{color: "red", fontSize: 72}}>Big Red</div>
+    );
+  }
+};
